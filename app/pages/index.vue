e5e0987a14d208033a75b9e2359f113ddb10d69f
EfraimGENC <efraim@kavimdigital.com>
2025-10-22 00:02:53 +0300

    feat: enhance Ebu Eyyûb el-Ensârî section with dynamic content and localization

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+import type { Collections } from '@nuxt/content'
+
+const route = useRoute()
+const { locale, t, defaultLocale, locales } = useI18n()
 const localePath = useLocalePath()
 
 useSeoMeta({
@@ -8,15 +12,54 @@ useSeoMeta({
 useHead({
   titleTemplate: '%s'
 })
+
+const personDetailRoute = (person: any) => {
+  const slug = person.path.split('/').pop()
+  return localePath({ name: 'person-slug', params: { slug } })
+}
+
+// Route'dan locale çıkarıyoruz - prerender sırasında daha güvenilir
+const currentLocale = computed(() => {
+  const fullPath = route.fullPath || route.path
+
+  for (const loc of locales.value) {
+    if (loc.code !== defaultLocale && fullPath.startsWith(`/${loc.code}/`)) {
+      return loc.code
+    }
+  }
+
+  return defaultLocale
+})
+
+const slug = "ebu-eyyub-el-ensari"
+const contentPath = `/person/${slug}`
+
+const { data: ebuEyyubElEnsari } = await useAsyncData(`person-${currentLocale.value}-${slug}`, async () => {
+
+  const getPeopleCollectionName = (locale: string = 'tr') => {
+    return (`people_${locale}`) as keyof Collections
+  }
+
+  const content = await queryCollection(getPeopleCollectionName(currentLocale.value)).path(contentPath).first();
+
+  if (!content && currentLocale.value !== 'tr') {
+    const fallbackContent = await queryCollection(getPeopleCollectionName()).path(contentPath).first();
+    return fallbackContent;
+  }
+
+  return content;
+}, {
+  watch: [locale],
+});
 </script>
 
 <template>
   <div>
     <!-- Hero Section -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24">
+    <div class="py-24">
 
       <!-- Hero Content -->
-      <div class="text-center">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
         <h1 class="text-4xl font-bold sm:text-5xl md:text-6xl">
           {{ $t('layouts.title') }}
         </h1>
@@ -27,7 +70,7 @@ useHead({
           <UButton :to="localePath('about')" variant="outline" color="neutral" size="lg" icon="tabler:info-circle">
             {{ $t('about.title') }}
           </UButton>
-          <UButton :to="localePath('person')" variant="outline" color="neutral" size="lg"
+          <UButton :to="localePath('person')" variant="solid" color="primary" size="lg"
             trailing-icon="i-tabler-arrow-right">
             {{ $t('nav.people') }}
           </UButton>
@@ -36,26 +79,24 @@ useHead({
 
       <!-- Under construction warning -->
       <UAlert :title="$t('layouts.warning.title')" :description="$t('layouts.warning.description')"
-        icon="tabler:info-triangle" variant="soft" color="warning" class="mt-10" />
+        icon="tabler:info-triangle" variant="soft" color="warning" class="mt-14" />
 
       <!-- Ebu Eyyûb el-Ensârî Hazretleri -->
       <div
         class="mt-4 p-2 rounded-2xl bg-gray-50/20 dark:bg-gray-900/20 border border-gray-200 dark:border-gray-800 flex flex-cols gap-3">
         <div>
-          <img src="/person/ebu-eyyub-el-ensari.jpg" alt="Ebedi Eyüpsultanlılar Çeşmesi"
+          <img :src="ebuEyyubElEnsari?.cover" alt="Ebedi Eyüpsultanlılar Çeşmesi"
             class="rounded-xl max-h-48 max-w-48" />
         </div>
         <div class="flex flex-col justify-between pt-1">
           <div class="">
-            <p class="text-muted text-sm leading-4 mb-1">Bu beldenin asıl sahibi, Allah Resûlü’nün mihmandarı</p>
-            <h4 class="m-0">Ebu Eyyûb el-Ensârî</h4>
-            <p class="leading-5 mb-0 line-clamp-3 md:line-clamp-4">Hicret'ten sonra Hz. Muhammed'i Medine'deki evinde
-              misafir eden ve
-              İstanbul'un fethinde manevi bir rol oynayan sahabe.</p>
+            <p class="text-muted text-sm leading-4 mb-1">{{ $t('about.ebuEyyubElEnsariDefinition') }}</p>
+            <h4 class="m-0">{{ ebuEyyubElEnsari?.title }}</h4>
+            <p class="leading-5 mb-0 line-clamp-3 md:line-clamp-4">{{ ebuEyyubElEnsari?.description }}</p>
           </div>
           <div class="flex justify-end">
-            <UButton :to="localePath({ name: 'person-slug', params: { slug: 'ebu-eyyub-el-ensari' } })" variant="subtle"
-              color="neutral" size="sm" trailing-icon="i-tabler-arrow-right">
+            <UButton :to="personDetailRoute(ebuEyyubElEnsari)" variant="subtle" color="neutral" size="sm"
+              trailing-icon="i-tabler-arrow-right">
               {{ $t('person.readMore') }}
             </UButton>
           </div>
